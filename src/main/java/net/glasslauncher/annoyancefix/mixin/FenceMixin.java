45b7c8db3f2e1eff542323ca7aabb8c0e655d7a2
2zqa <25235249+2zqa@users.noreply.github.com>
2023-07-30 17:29:59 +0200

    Remove old files, make fences placeable everywhere

---
+++ src/main/java/net/glasslauncher/annoyancefix/mixin/FenceMixin.java
@@ -0,0 +1,29 @@
+package net.glasslauncher.annoyancefix.mixin;
+
+import net.minecraft.block.BlockBase;
+import net.minecraft.block.Fence;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Block;
+import net.minecraft.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Fence.class)
+public abstract class FenceMixin extends BlockBase {
+
+    public FenceMixin(int i, Material arg) {
+        super(i, arg);
+    }
+
+    public FenceMixin(int i, int j, Material arg) {
+        super(i, j, arg);
+    }
+
+    @Inject(at = @At("RETURN"), method = "canPlaceAt", cancellable = true)
+    public void canPlaceAt(Level arg, int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
+
