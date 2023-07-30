2359686db6aba3f692066fd4d4c0096e9894204d
telvarost <132113523+telvarost@users.noreply.github.com>
2023-07-30 16:35:25 -0400

    ISSUE-4 - Fix broken recipes; Test #4 in commit message

--- src/main/java/net/glasslauncher/annoyancefix/events/init/RecipeListener.java
+++ src/main/java/net/glasslauncher/annoyancefix/events/init/RecipeListener.java
@@ -15,15 +15,14 @@ public class RecipeListener {
     public void registerRecipes(RecipeRegisterEvent event) {
 
         Identifier type = event.recipeId;
-//        if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED.type()) {
-//            CraftingRegistry.addShapedRecipe(new ItemInstance(ItemListener.coolItem, 1), "XXX", "X X", "X X", 'X', BlockBase.DIRT);
-//        }
+        if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED.type()) {
+            CraftingRegistry.addShapedRecipe(new ItemInstance(ItemBase.apple, 1), "XXX", "X X", "X X", 'X', BlockBase.DIRT);
+        }
         if (type == RecipeRegisterEvent.Vanilla.SMELTING.type()) {
             SmeltingRegistry.addSmeltingRecipe(new ItemInstance(ItemBase.apple, 1), new ItemInstance(BlockBase.WOOL));
         }
         if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type()) {
-            CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.LOG, 1), new ItemInstance(BlockBase.DIRT));
-            CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.WOOD, 1), new ItemInstance(BlockBase.DIRT), new ItemInstance(BlockBase.DIRT));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.ironChestplate.id, 1, 10), new ItemInstance(ItemBase.ironChestplate), new ItemInstance(ItemBase.ironIngot));
         }
     }
 }
