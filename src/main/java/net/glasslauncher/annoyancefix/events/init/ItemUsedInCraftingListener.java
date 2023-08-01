3c7ad0f15049e222c18c37ad4061f38e66dce2a3
telvarost <132113523+telvarost@users.noreply.github.com>
2023-07-31 20:00:30 -0400

    ISSUE-4 - Allow armour to combine durability when crafting two broken pieces together

---
+++ src/main/java/net/glasslauncher/annoyancefix/events/init/ItemUsedInCraftingListener.java
@@ -0,0 +1,32 @@
+package net.glasslauncher.annoyancefix.events.init;
+
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.modificationstation.stationapi.api.event.container.slot.ItemUsedInCraftingEvent;
+
+public class ItemUsedInCraftingListener {
+
+    @EventListener
+    public void combineDurability(ItemUsedInCraftingEvent event) {
+        if (  (null != event.itemUsed)
+           && (true == event.itemUsed.hasDurability())
+           && (null != event.itemCrafted)
+           && (true == event.itemCrafted.hasDurability())
+           )
+        {
+            int durabilityToAdd = event.itemUsed.getDurability() - event.itemUsed.getDamage();
+            int newDurability = event.itemCrafted.getDurability() - event.itemCrafted.getDamage();
+            int damageToSet = 0;
+
+            newDurability = newDurability + durabilityToAdd;
+
+            /** - Only calculate damage if new durability is below max durability */
+            if (event.itemCrafted.getDurability() > newDurability)
+            {
+                /** - Max durability - new durability = damage to apply to max durability */
+                damageToSet = event.itemCrafted.getDurability() - newDurability;
+            }
+
+            event.itemCrafted.setDamage(damageToSet);
+        }
+    }
+}
--- src/main/java/net/glasslauncher/annoyancefix/events/init/RecipeListener.java
+++ src/main/java/net/glasslauncher/annoyancefix/events/init/RecipeListener.java
@@ -13,16 +13,28 @@ public class RecipeListener {
 
     @EventListener
     public void registerRecipes(RecipeRegisterEvent event) {
-
         Identifier type = event.recipeId;
-        if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED.type()) {
-            CraftingRegistry.addShapedRecipe(new ItemInstance(ItemBase.apple, 1), "XXX", "X X", "X X", 'X', BlockBase.DIRT);
-        }
-        if (type == RecipeRegisterEvent.Vanilla.SMELTING.type()) {
-            SmeltingRegistry.addSmeltingRecipe(new ItemInstance(ItemBase.apple, 1), new ItemInstance(BlockBase.WOOL));
-        }
+
         if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type()) {
-            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.ironChestplate.id, 1, 10), new ItemInstance(ItemBase.ironChestplate), new ItemInstance(ItemBase.ironIngot));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.leatherHelmet.id, 1, ItemBase.leatherHelmet.getDurability()), new ItemInstance(ItemBase.leatherHelmet.id, 1, -1), new ItemInstance(ItemBase.leatherHelmet.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.leatherChestplate.id, 1, ItemBase.leatherChestplate.getDurability()), new ItemInstance(ItemBase.leatherChestplate.id, 1, -1), new ItemInstance(ItemBase.leatherChestplate.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.leatherLeggings.id, 1, ItemBase.leatherLeggings.getDurability()), new ItemInstance(ItemBase.leatherLeggings.id, 1, -1), new ItemInstance(ItemBase.leatherLeggings.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.leatherBoots.id, 1, ItemBase.leatherBoots.getDurability()), new ItemInstance(ItemBase.leatherBoots.id, 1, -1), new ItemInstance(ItemBase.leatherBoots.id, 1, -1));
+
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.chainHelmet.id, 1, ItemBase.chainHelmet.getDurability()), new ItemInstance(ItemBase.chainHelmet.id, 1, -1), new ItemInstance(ItemBase.chainHelmet.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.chainChestplate.id, 1, ItemBase.chainChestplate.getDurability()), new ItemInstance(ItemBase.chainChestplate.id, 1, -1), new ItemInstance(ItemBase.chainChestplate.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.chainLeggings.id, 1, ItemBase.chainLeggings.getDurability()), new ItemInstance(ItemBase.chainLeggings.id, 1, -1), new ItemInstance(ItemBase.chainLeggings.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.chainBoots.id, 1, ItemBase.chainBoots.getDurability()), new ItemInstance(ItemBase.chainBoots.id, 1, -1), new ItemInstance(ItemBase.chainBoots.id, 1, -1));
+
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.ironHelmet.id, 1, ItemBase.ironHelmet.getDurability()), new ItemInstance(ItemBase.ironHelmet.id, 1, -1), new ItemInstance(ItemBase.ironHelmet.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.ironChestplate.id, 1, ItemBase.ironChestplate.getDurability()), new ItemInstance(ItemBase.ironChestplate.id, 1, -1), new ItemInstance(ItemBase.ironChestplate.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.ironLeggings.id, 1, ItemBase.ironLeggings.getDurability()), new ItemInstance(ItemBase.ironLeggings.id, 1, -1), new ItemInstance(ItemBase.ironLeggings.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.ironBoots.id, 1, ItemBase.ironBoots.getDurability()), new ItemInstance(ItemBase.ironBoots.id, 1, -1), new ItemInstance(ItemBase.ironBoots.id, 1, -1));
+
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.diamondHelmet.id, 1, ItemBase.diamondHelmet.getDurability()), new ItemInstance(ItemBase.diamondHelmet.id, 1, -1), new ItemInstance(ItemBase.diamondHelmet.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.diamondChestplate.id, 1, ItemBase.diamondChestplate.getDurability()), new ItemInstance(ItemBase.diamondChestplate.id, 1, -1), new ItemInstance(ItemBase.diamondChestplate.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.diamondLeggings.id, 1, ItemBase.diamondLeggings.getDurability()), new ItemInstance(ItemBase.diamondLeggings.id, 1, -1), new ItemInstance(ItemBase.diamondLeggings.id, 1, -1));
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.diamondBoots.id, 1, ItemBase.diamondBoots.getDurability()), new ItemInstance(ItemBase.diamondBoots.id, 1, -1), new ItemInstance(ItemBase.diamondBoots.id, 1, -1));
         }
     }
 }
