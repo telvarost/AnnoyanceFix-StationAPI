4adc6b338d88decf5e9ee885b9909b4f289b8b62
mineLdiver <aabesedin@mail.ru>
2023-08-06 21:39:56 +0500

    Improve FenceMixin

--- src/main/java/net/glasslauncher/annoyancefix/mixin/FenceMixin.java
+++ src/main/java/net/glasslauncher/annoyancefix/mixin/FenceMixin.java
@@ -9,19 +9,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = Fence.class)
-public abstract class FenceMixin extends BlockBase {
-
+@Mixin(Fence.class)
+abstract class FenceMixin extends BlockBase {
     public FenceMixin(int i, Material arg) {
         super(i, arg);
     }
 
-    public FenceMixin(int i, int j, Material arg) {
-        super(i, j, arg);
-    }
-
-    @Inject(at = @At("RETURN"), method = "canPlaceAt", cancellable = true)
-    public void canPlaceAt(Level arg, int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(
+            method = "canPlaceAt",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void annoyancefix_canPlaceAt(Level arg, int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
     }
 }
