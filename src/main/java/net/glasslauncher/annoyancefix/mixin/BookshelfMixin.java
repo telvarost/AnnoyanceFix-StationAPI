bc9aee72871c22f19cbc84be1ae201ffb81cc855
telvarost <132113523+telvarost@users.noreply.github.com>
2023-08-06 08:39:29 -0400

    ISSUE-7 - Remove disused imports

--- src/main/java/net/glasslauncher/annoyancefix/mixin/BookshelfMixin.java
+++ src/main/java/net/glasslauncher/annoyancefix/mixin/BookshelfMixin.java
@@ -3,16 +3,11 @@ package net.glasslauncher.annoyancefix.mixin;
 
 import net.minecraft.block.BlockBase;
 import net.minecraft.block.Bookshelf;
-import net.minecraft.block.Stairs;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemBase;
-import net.minecraft.item.ItemInstance;
-import net.minecraft.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
