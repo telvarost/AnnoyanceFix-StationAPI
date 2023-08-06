a2982d552b2803f3e63e4a7ecd6b82923eae6714
mineLdiver <aabesedin@mail.ru>
2023-08-06 21:46:08 +0500

    Improve HatchetMixin

--- src/main/java/net/glasslauncher/annoyancefix/mixin/HatchetMixin.java
+++ src/main/java/net/glasslauncher/annoyancefix/mixin/HatchetMixin.java
@@ -1,36 +1,50 @@
 package net.glasslauncher.annoyancefix.mixin;
 
+import com.google.common.collect.ObjectArrays;
 import net.minecraft.block.BlockBase;
 import net.minecraft.item.tool.Hatchet;
 import net.minecraft.item.tool.ToolBase;
 import net.minecraft.item.tool.ToolMaterial;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Hatchet.class)
-public abstract class HatchetMixin extends ToolBase
-{
-    @Shadow private static BlockBase[] effectiveBlocks = new BlockBase[]
-            { BlockBase.WOOD
-            , BlockBase.BOOKSHELF
-            , BlockBase.LOG
-            , BlockBase.CHEST
-            , BlockBase.WORKBENCH
-            , BlockBase.NOTEBLOCK
-            , BlockBase.WOOD_DOOR
-            , BlockBase.LADDER
-            , BlockBase.STANDING_SIGN
-            , BlockBase.WALL_SIGN
-            , BlockBase.WOODEN_PRESSURE_PLATE
-            , BlockBase.JUKEBOX
-            , BlockBase.WOOD_STAIRS
-            , BlockBase.FENCE
-            , BlockBase.PUMPKIN
-            , BlockBase.JACK_O_LANTERN
-            , BlockBase.TRAPDOOR
-            };
+class HatchetMixin extends ToolBase {
+    @Shadow private static BlockBase[] effectiveBlocks;
 
-    protected HatchetMixin(int i, ToolMaterial arg) {
-        super(i, 3, arg, effectiveBlocks);
+    public HatchetMixin(int i, int j, ToolMaterial arg, BlockBase[] args) {
+        super(i, j, arg, args);
+    }
+
+    @Inject(
+            method = "<clinit>",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/item/tool/Hatchet;effectiveBlocks:[Lnet/minecraft/block/BlockBase;",
+                    opcode = Opcodes.PUTSTATIC,
+                    shift = At.Shift.AFTER
+            )
+    )
+    private static void annoyancefix_appendExtraBlocks(CallbackInfo ci) {
+        effectiveBlocks = ObjectArrays.concat(effectiveBlocks, new BlockBase[]
+                { BlockBase.WORKBENCH
+                , BlockBase.NOTEBLOCK
+                , BlockBase.WOOD_DOOR
+                , BlockBase.LADDER
+                , BlockBase.STANDING_SIGN
+                , BlockBase.WALL_SIGN
+                , BlockBase.WOODEN_PRESSURE_PLATE
+                , BlockBase.JUKEBOX
+                , BlockBase.WOOD_STAIRS
+                , BlockBase.FENCE
+                , BlockBase.PUMPKIN
+                , BlockBase.JACK_O_LANTERN
+                , BlockBase.TRAPDOOR
+        }, BlockBase.class);
     }
 }
