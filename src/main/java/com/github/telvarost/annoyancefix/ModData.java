30f5a58ed63675aea942e141805b69126d5d282c
Marijn <25235249+2zqa@users.noreply.github.com>
2023-09-17 14:40:36 +0200

    Make wooden slabs breakable by hand (#39)
    * WIP make hands work on wooden slabs
    
    * Fix styling, remove unnecessary code
    
    * Rename isStoneSlab
    
    Co-authored-by: Telvarost <132113523+telvarost@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Telvarost <132113523+telvarost@users.noreply.github.com>

--- src/main/java/com/github/telvarost/annoyancefix/ModData.java
+++ src/main/java/com/github/telvarost/annoyancefix/ModData.java
@@ -3,6 +3,6 @@ package com.github.telvarost.annoyancefix;
 public class ModData {
     public static class ModDataFields {
         /** @brief - Special flag for determining if a slab is wooden or not */
-        public static Boolean isBlockMetaDataValue2 = false;
+        public static boolean isBlockMetaDataValue2 = false;
     }
 }
