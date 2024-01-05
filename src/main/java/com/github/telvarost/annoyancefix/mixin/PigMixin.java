dad79e18c3205194cb60b9677b2fcf7a54b58076
telvarost <132113523+telvarost@users.noreply.github.com>
2024-01-05 17:24:42 -0500

    ISSUE-51 - Allow pigs to drop saddles when saddles on pig death

---
+++ src/main/java/com/github/telvarost/annoyancefix/mixin/PigMixin.java
@@ -0,0 +1,41 @@
+package com.github.telvarost.annoyancefix.mixin;
+
+import net.minecraft.entity.animal.AnimalBase;
+import net.minecraft.entity.animal.Pig;
+import net.minecraft.item.ItemBase;
+import net.minecraft.level.Level;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Pig.class)
+public abstract class PigMixin extends AnimalBase {
+
+    @Shadow public abstract boolean isSaddled();
+
+    public PigMixin(Level arg) {
+        super(arg);
+        this.texture = "/mob/pig.png";
+        this.setSize(0.9F, 0.9F);
+    }
+
+    @Override
+    protected void getDrops() {
+        int var1 = this.getMobDrops();
+        if (var1 > 0) {
+            int var2 = this.rand.nextInt(3);
+
+            for(int var3 = 0; var3 < var2; ++var3) {
+                this.dropItem(var1, 1);
+            }
+
+            if (this.isSaddled())
+            {
+                this.dropItem(ItemBase.saddle.id, 1);
+            }
+        }
+    }
+}
