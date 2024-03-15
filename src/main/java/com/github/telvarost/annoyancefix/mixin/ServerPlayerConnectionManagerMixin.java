d72929d44502fa8e2ddcfdfe43d8a8f3398981de
telvarost <132113523+telvarost@users.noreply.github.com>
2024-03-14 20:41:11 -0400

    ISSUE-75 - Remove vehicle on log out

---
+++ src/main/java/com/github/telvarost/annoyancefix/mixin/ServerPlayerConnectionManagerMixin.java
@@ -0,0 +1,43 @@
+package com.github.telvarost.annoyancefix.mixin;
+
+import net.minecraft.entity.player.ServerPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerHandler;
+import net.minecraft.server.ServerPlayerConnectionManager;
+import net.minecraft.server.ServerPlayerView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(ServerPlayerConnectionManager.class)
+public abstract class ServerPlayerConnectionManagerMixin {
+
+
+    @Shadow private PlayerHandler field_590;
+
+    @Shadow private MinecraftServer server;
+
+    @Shadow public List players;
+
+    @Shadow protected abstract ServerPlayerView getPlayerView(int i);
+
+    @Inject(
+            method = "updateDimension",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void updateDimension(ServerPlayer arg, CallbackInfo ci) {
+        if (null != arg.vehicle) {
+            arg.level.removeEntity(arg.vehicle);
+        }
+        this.field_590.savePlayer(arg);
+        this.server.getLevel(arg.dimensionId).removeEntity(arg);
+        this.players.remove(arg);
+        this.getPlayerView(arg.dimensionId).addPlayer(arg);
+        ci.cancel();
+    }
+}
