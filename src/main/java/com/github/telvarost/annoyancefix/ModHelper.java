ab9d25c1497825bbedbdc5c4b878fba58aa5e77f
telvarost <132113523+telvarost@users.noreply.github.com>
2024-04-12 22:44:07 -0400

    ISSUE-75 - Fix finding saved vehicle on login

--- src/main/java/com/github/telvarost/annoyancefix/ModHelper.java
+++ src/main/java/com/github/telvarost/annoyancefix/ModHelper.java
@@ -22,5 +22,12 @@ public class ModHelper {
 
         /** @brief - Special data for flint and steel fixes */
         public static Boolean isFireLit = false;
+
+        /** @brief - Special data for finding vehicle on login */
+        public static Boolean isVehicleSaved = false;
+        public static Class savedVehicleClass = null;
+        public static Double savedVehicleX = 0.0D;
+        public static Double savedVehicleY = 0.0D;
+        public static Double savedVehicleZ = 0.0D;
     }
 }
