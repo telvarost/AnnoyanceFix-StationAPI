d8e20640fcafc0bd3b48b662d911fff0c121f2c1
telvarost <132113523+telvarost@users.noreply.github.com>
2024-04-19 16:49:03 -0400

    UDOC-0 - Improve compatibility of AnnoyanceFix by removing override in pig mixin and using a non-invasive inject instead

--- src/main/java/com/github/telvarost/annoyancefix/mixin/PigMixin.java
+++ src/main/java/com/github/telvarost/annoyancefix/mixin/PigMixin.java
@@ -5,12 +5,11 @@ import net.minecraft.entity.animal.AnimalBase;
 import net.minecraft.entity.animal.Pig;
 import net.minecraft.item.ItemBase;
 import net.minecraft.level.Level;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Pig.class)
 public abstract class PigMixin extends AnimalBase {
@@ -23,22 +22,16 @@ public abstract class PigMixin extends AnimalBase {
         this.setSize(0.9F, 0.9F);
     }
 
-    @Override
-    protected void getDrops() {
-        int var1 = this.getMobDrops();
-        if (var1 > 0) {
-            int var2 = this.rand.nextInt(3);
-
-            for(int var3 = 0; var3 < var2; ++var3) {
-                this.dropItem(var1, 1);
-            }
-
-            if (  (Config.config.pigFixesEnabled)
-               && (this.isSaddled())
-               )
-            {
-                this.dropItem(ItemBase.saddle.id, 1);
-            }
+    @Inject(
+            method = "getMobDrops",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void annoyanceFix_getMobDrops(CallbackInfoReturnable<Integer> cir) {
+        if (  (Config.config.pigFixesEnabled)
+           && (this.isSaddled())
+        ) {
+            this.dropItem(ItemBase.saddle.id, 1);
         }
     }
 }
