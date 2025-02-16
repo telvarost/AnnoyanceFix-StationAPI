496cfa133dc4e7285679b54eef9b0e7f44a8d964
telvarost <telontelvarost@gmail.com>
2025-02-15 22:56:41 -0500

    Fix double wooden slab mining speed

--- src/main/java/com/github/telvarost/annoyancefix/events/MiningListener.java
+++ src/main/java/com/github/telvarost/annoyancefix/events/MiningListener.java
@@ -37,13 +37,21 @@ public class MiningListener {
         int blockMeta = world.getBlockMeta(pos.x, pos.y, pos.z);
 
         // If the ID and Meta matches, true
-        if(blockMeta == 2 && blockId == Block.SLAB.id){
+        if (  blockMeta == 2
+           && (  blockId == Block.SLAB.id
+              || blockId == Block.DOUBLE_SLAB.id
+              )
+        ) {
             event.resultProvider = () -> true;
 
             // If it doesnt, it might be last breaking tick and check the last values
         } else if (blockMeta == 0 && blockId == 0) {
             // If in the last tick it matched, true
-            if (lastBlockMeta == 2 && lastBlockId == Block.SLAB.id) {
+            if ( lastBlockMeta == 2
+               && (  lastBlockId == Block.SLAB.id
+                  || lastBlockId == Block.DOUBLE_SLAB.id
+                  )
+            ) {
                 event.resultProvider = () -> true;
             }
         }
@@ -66,7 +74,9 @@ public class MiningListener {
 
         // If the ID and Meta matches, true
         if (  world.getBlockMeta(pos.x, pos.y, pos.z) == 2
-           && world.getBlockId(pos.x, pos.y, pos.z) == Block.SLAB.id
+           && (  world.getBlockId(pos.x, pos.y, pos.z) == Block.SLAB.id
+              || world.getBlockId(pos.x, pos.y, pos.z) == Block.DOUBLE_SLAB.id
+              )
            && null != event.player.inventory.getSelectedItem()
         ) {
             if(event.player.inventory.getSelectedItem().getItem() instanceof AxeItem axe){
